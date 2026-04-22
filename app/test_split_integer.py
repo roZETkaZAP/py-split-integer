23d249fa17550bd7f677db40fd96557a3736303a
Igor <zalugin.iv2@gmail.com>
2026-04-22 22:20:01 +0200

    changes added

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -6,7 +6,7 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert split_integer(10, 2) == [5, 5]
+    assert split_integer(16, 4) == [4, 4, 4, 4]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
@@ -14,13 +14,13 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    result = split_integer(22, 3)
-    assert result == sorted(result)
+    assert split_integer(22, 3) == sorted(split_integer(22, 3))
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert len(split_integer(1, 3)) == 3
+    assert split_integer(3, 4) == [0, 1, 1, 1]
 
 
-def test_min_and_max_differance() -> None:
-    assert split_integer(17, 4) == [4, 4, 4, 5]
+def test_min_and_max_difference() -> None:
+    result = split_integer(17, 4)
+    assert max(result) - min(result) <= 1
